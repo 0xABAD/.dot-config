efcb0b1528897002051a40930cecce457732f694
Dan Abad <dabad@slb.com>
2019-09-21 17:02:13 -0500

    Remove init.el and its setup
    The init.el file has been moved to a separate repo soley for managing
    Emacs.

--- emacs.zsh
+++ emacs.zsh
@@ -8,15 +8,3 @@ fi
 if [[ $(ps -ef | grep "emacs --daemon" | grep -v "grep" | wc -l) -eq 0 ]] ; then
     emacs --daemon
 fi
-
-# Check if the emacs config directory exist and if not then download comma-mode
-# and copy over the init.el file.
-if [[ ! -d "$HOME/.emacs.d" ]] ; then
-    mkdir -p "$HOME/.emacs.d"
-    cp "$HOME/.dot-config/init.el" "$HOME/.emacs.d/"
-    git clone "https://github.com/0xABAD/comma-mode" "$HOME/.emacs.d/lisp"
-# Check if init.el is newer than the saved one in .dot-config in order to update the git repo.
-elif [[ "$HOME/.emacs.d/init.el" -nt "$HOME/.dot-config/init.el" ]] ; then
-    cp "$HOME/.emacs.d/init.el" "$HOME/.dot-config/"
-    echo "Emacs init.el updated in .dot-config directory.  Update git repo."
-fi
